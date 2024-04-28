f0e124a989fa6421641f7c242c000e13ef226a34
Melina Moraiti <melinamoraith@gmail.com>
2024-04-28 17:11:18 +0300

    bugfix: Navigation region 2d is detected

---
+++ Characters/Enemies/Idle.gd
@@ -0,0 +1,14 @@
+class_name Idle
+extends State
+
+func Enter() -> void:
+	pass
+	
+func Exit() -> void:
+	pass
+	
+func Update(delta: float) -> void:
+	pass
+ 
+func Physics_update(delta: float) -> void:
+	pass
--- Scripts/States/Patrol.gd
+++ Scripts/States/Patrol.gd
@@ -1,6 +1,6 @@
 class_name Patrol
 extends State
-var collision : CollisionShape2D
+var detectionCollision : CollisionShape2D
 @export var starting_direction : Vector2 = Vector2(0, 1)
 @onready var animation_tree = $"../../AnimationTree"
 @onready var state_machine = animation_tree.get("parameters/playback")
@@ -14,7 +14,7 @@ func Enter():
 	player_in_zone = false
 	target = get_tree().get_first_node_in_group("Player")
 	enemy =  $"../.."
-	collision = $"../../detection_zone/CollisionShape2D"
+	detectionCollision = $"../../detection_zone/CollisionShape2D"
 	update_animation_parameters(starting_direction)
 func Exit():
 	returns_to_path = true
@@ -25,7 +25,7 @@ func Physics_update(delta):
 	if returns_to_path == true:
 		generate_path()
 		var direction = enemy.to_local(nav_agent.get_next_path_position()).normalized()
-		enemy.velocity = direction * enemy.move_speed
+		enemy.velocity = direction * enemy.move_speed 
 		update_animation_parameters(direction)
 		enemy.move_and_slide()
 		pick_new_animation()
@@ -35,14 +35,16 @@ func update_animation_parameters(move_direction : Vector2):
 	if(move_direction != Vector2.ZERO):
 		animation_tree.set("parameters/Idle/blend_position", move_direction)
 		animation_tree.set("parameters/Move/blend_position", move_direction)
+# It generates the return path
 func generate_path() -> void:
 	if path_follow != null:
-		nav_agent.target_position = path_follow.global_position
+		nav_agent.target_position = path_follow.position
 func pick_new_animation():
 	if(enemy.velocity != Vector2.ZERO):
 		state_machine.travel("Move")
 	else:
 		state_machine.travel("Idle")
+#
 func _on_detection_zone_body_entered(body):
 	if body.has_method("player"):
 		player_in_zone = true
