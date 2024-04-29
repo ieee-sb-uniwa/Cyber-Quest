2f421542049541c78ad2c64e602d606aa62efb4e
Melina Moraiti <melinamoraith@gmail.com>
2024-04-29 16:39:20 +0300

    Change direction and velocity when returning to path

--- Scripts/States/Patrol.gd
+++ Scripts/States/Patrol.gd
@@ -1,31 +1,26 @@
 class_name Patrol
 extends State
-var detectionCollision : CollisionShape2D
 @export var starting_direction : Vector2 = Vector2(0, 1)
 @onready var animation_tree = $"../../AnimationTree"
 @onready var state_machine = animation_tree.get("parameters/playback")
 @onready var nav_agent := $"../../NavigationAgent2D" as NavigationAgent2D
-@onready var path_follow : PathFollow2D = $"../../../PatrolPaths/Path2D/PathFollow2D"
+@export var enemy : CharacterBody2D
 var player_in_zone: bool
 var returns_to_path: bool
-var target : CharacterBody2D
-var enemy : CharacterBody2D
 func Enter():
 	player_in_zone = false
-	target = get_tree().get_first_node_in_group("Player")
 	enemy =  $"../.."
-	detectionCollision = $"../../detection_zone/CollisionShape2D"
 	update_animation_parameters(starting_direction)
 func Exit():
 	returns_to_path = true
 func Update(delta : float) -> void:
 	if returns_to_path == false:
-		path_follow.progress += delta * enemy.move_speed
-func Physics_update(delta):
+		enemy.path_follow.progress += delta * enemy.move_speed
+func Physics_update(_delta : float):
 	if returns_to_path == true:
 		generate_path()
-		var direction = enemy.to_local(nav_agent.get_next_path_position()).normalized()
-		enemy.velocity = direction * enemy.move_speed 
+		var direction = (nav_agent.get_next_path_position() - enemy.position).normalized()
+		enemy.velocity = enemy.velocity.lerp(direction * enemy.move_speed,enemy.acceleration * _delta)
 		update_animation_parameters(direction)
 		enemy.move_and_slide()
 		pick_new_animation()
@@ -37,8 +32,8 @@ func update_animation_parameters(move_direction : Vector2):
 		animation_tree.set("parameters/Move/blend_position", move_direction)
 # It generates the return path
 func generate_path() -> void:
-	if path_follow != null:
-		nav_agent.target_position = path_follow.position
+	if enemy.path_follow != null:
+		nav_agent.target_position = enemy.path_follow.position
 func pick_new_animation():
 	if(enemy.velocity != Vector2.ZERO):
 		state_machine.travel("Move")
@@ -48,6 +43,6 @@ func pick_new_animation():
 func _on_detection_zone_body_entered(body):
 	if body.has_method("player"):
 		player_in_zone = true
-		target = body
+		enemy.hunting_target = body
 func _on_navigation_agent_2d_target_reached():
 	returns_to_path=false
