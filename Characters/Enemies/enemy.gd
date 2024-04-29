f684ce3966076a2db2eff03e857c6efd15f76288
Melina Moraiti <melinamoraith@gmail.com>
2024-04-29 16:36:54 +0300

    Add export variables for flexibilty in multiple enemies

--- Characters/Enemies/enemy.gd
+++ Characters/Enemies/enemy.gd
@@ -1,9 +1,12 @@
 extends CharacterBody2D
 class_name Enemy
-@export var move_speed : float = 50
+@export var move_speed : float = 300
+@export var acceleration : float = 7
+@export var path_follow : PathFollow2D 
+@export var hunting_target : CharacterBody2D 
 var player_dead = false
 func _physics_process(_delta):
 	if !player_dead :
-		$detection_zone/CollisionShape2D.disabled = false
+		$detection_zone/Circle.disabled = false
 	else:
-		$detection_zone/CollisionShape2D.disabled = true;
+		$detection_zone/Circle.disabled = true
