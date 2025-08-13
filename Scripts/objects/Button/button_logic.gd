c6ecdee5c7c30206757c0345452dbe47c0da458e
frottori <frossovarsou@gmail.com>
2025-08-13 12:34:10 +0300

    Rename parameter in button press functions

--- Scripts/objects/Button/button_logic.gd
+++ Scripts/objects/Button/button_logic.gd
@@ -4,10 +4,10 @@ extends Node2D
 signal button_pressed()
 signal button_unpressed()
 
-func _on_button_pressed(body: Node2D) -> void:
+func _on_button_pressed(_body: Node2D) -> void:
 	sprite.frame = 15
 	emit_signal("button_pressed")
 
-func _on_button_unpressed(body: Node2D) -> void:
+func _on_button_unpressed(_body: Node2D) -> void:
 	sprite.frame = 14
 	emit_signal("button_unpressed")
