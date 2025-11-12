0bf4b09e1771d262d14a9f5d0085287c781c3b4c
frottori <frossovarsou@gmail.com>
2025-11-12 15:33:23 +0200

    minor fixes

--- Dialogues/Level_Dialogues/Balloons/example_balloons/balloon.gd
+++ Dialogues/Level_Dialogues/Balloons/example_balloons/balloon.gd
@@ -70,7 +70,7 @@ func _ready() -> void:
 	add_child(mutation_cooldown)
 
 
-func _process(delta: float) -> void:
+func _process(_delta: float) -> void:
 	progress.visible = not dialogue_label.is_typing and dialogue_line.responses.size() == 0 and not dialogue_line.has_tag("voice")
 
 
