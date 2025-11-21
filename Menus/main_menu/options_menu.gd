dc329205f83626f4c494347c7281826dffc50ca9
IM4tlas <serafimpar@gmail.com>
2025-11-21 18:18:29 +0200

    Added global audio-player, implemented menu_music loop and volume control

--- Menus/main_menu/options_menu.gd
+++ Menus/main_menu/options_menu.gd
@@ -4,6 +4,7 @@ extends Control;
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$VBoxContainer2/BacktoMenu.grab_focus();
+	$VBoxContainer/MusicSlider.value = db_to_linear(AudioPlayer.music_vol)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,20 +14,11 @@ func _process(_delta: float) -> void:
 func _on_backto_menu_pressed():
 	get_tree().change_scene_to_file("res://Menus/main_menu/Menu.tscn");
 
-
-
-func _on_master_slider_drag_ended(value_changed:bool) -> void:
-	pass # Replace with function body.
-
-
-func _on_music_slider_drag_ended(value_changed:bool) -> void:
-	pass
-
-func _on_sounds_slider_drag_ended(value_changed:bool) -> void:
-	pass # Replace with function body.
-
-
 func _on_resume_playing_pressed() -> void:
 	$"../InventoryGUI".show()
 	$"../../".show()
 	queue_free()	# Destroys the instanced options node
+
+
+func _on_music_slider_value_changed(value:float) -> void:
+	AudioPlayer.play_music_menu(linear_to_db(value))
