e32ac49d0780928019e1d6e99e1d436eb6a9bed9
Chrxb <60367797+Chrxb@users.noreply.github.com>
2025-12-03 16:05:30 +0200

    text updates from the start of the level

--- HUD/Scripts_Resources/passblock_counter.gd
+++ HUD/Scripts_Resources/passblock_counter.gd
@@ -3,6 +3,7 @@ extends Node
 
 func _ready():
 	Global.passblock_count_changed.connect(update_text)
+	update_text()
 
 func update_text():
 	passblock_text.text = "{0}/{1}".format([Global.passblock_count, Global.passblocks_in_level.size()])
