8ae1e7c4eda4f8c3b2ba54c40380f588b4cfa020
Chrxb <60367797+Chrxb@users.noreply.github.com>
2025-12-03 15:54:42 +0200

    passblock counter

---
+++ HUD/Scripts_Resources/passblock_counter.gd
@@ -0,0 +1,8 @@
+extends Node
+@onready var passblock_text : RichTextLabel = $PassblockCounterText
+
+func _ready():
+	Global.passblock_count_changed.connect(update_text)
+
+func update_text():
+	passblock_text.text = "{0}/{1}".format([Global.passblock_count, Global.passblocks_in_level.size()])
