9f53e9ae00f192eac024fcb7cd3bb9b3e6aa3be6
frottori <frossovarsou@gmail.com>
2025-12-20 17:02:36 +0200

    merge conflict fixes

--- Menus/pause_menu/HUD/Scripts_Resources/inventory_item.gd
+++
@@ -1,7 +0,0 @@
-extends Resource
-# δημιουργει ενα resource με τις παρακατω ιδιοτητες 
-class_name InventoryItem 
-# ονομα αντικειμενου
-@export var name : String = " " 
-# texture αντικειμενου
-@export var texture : Texture2D 
