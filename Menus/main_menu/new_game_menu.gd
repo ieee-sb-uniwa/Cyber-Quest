715c2c9684d3a1bdbbda02fa8f1477d6d17da330
frottori <frossovarsou@gmail.com>
2026-04-18 20:49:24 +0300

    not input greek username in new game

--- Menus/main_menu/new_game_menu.gd
+++ Menus/main_menu/new_game_menu.gd
@@ -1,46 +1,63 @@
 extends Control
 
-var DoB := RegEx.new()
+var date_of_birth := RegEx.new()
+var greek_letters := RegEx.new()
 var player_name_1: String
 var player_name_2: String
 var birth_date_: String
 var birthdate_2: String
 
+@onready var name_input: LineEdit = $CenterContainer/Menu/InputForm/NameIN
+@onready var date_input: LineEdit = $CenterContainer/Menu/InputForm/DateIN
+@onready var menu_container: Control = $CenterContainer/Menu
+@onready var popup: Control = $"CenterContainer/Pop-Up"
+@onready var error_label: Label = $"CenterContainer/Pop-Up/ErrorLabel"
+@onready var player_label: Label = $CenterContainer/Menu/PlayerLabel
+@onready var confirm_button: Button = $CenterContainer/Menu/Buttons/Confirm
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$CenterContainer/Menu/InputForm/NameIN.grab_focus()
+	name_input.grab_focus()
+	greek_letters.compile("[Α-Ωα-ω]")
+	date_of_birth.compile("[A-Za-z]\\w")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
+func show_error(message: String) -> void:
+	menu_container.visible = false
+	popup.visible = true
+	error_label.text = message
+
 func _on_confirm_pressed() -> void:
 
-	DoB.compile("[A-Za-z]\\w")
-	var result := DoB.search($CenterContainer/Menu/InputForm/DateIN.text)
+	# Check for Greek letters in name
+	var greek_check := greek_letters.search(name_input.text)
+	if greek_check:
+		show_error("Το όνομα δεν μπορεί να περιέχει ελληνικά γράμματα!")
+		return
+	
+	var result := date_of_birth.search(date_input.text)
 	if result:
-		$CenterContainer/Menu.visible = false
-		$"CenterContainer/Pop-Up".visible = true
-		$"CenterContainer/Pop-Up/ErrorLabel".text = "Η Ημερομηνία γέννησης να έχει μορφή: 01/01/0001 !"
+		show_error("Η Ημερομηνία γέννησης να έχει μορφή: 01/01/0001 !")
 		return
 		
-	if $CenterContainer/Menu/InputForm/DateIN.text.length() != 10 || $CenterContainer/Menu/InputForm/NameIN.text.length() == 0:
-		$CenterContainer/Menu.visible = false
-		$"CenterContainer/Pop-Up".visible = true
-		$"CenterContainer/Pop-Up/ErrorLabel".text = "Παρακαλώ εισάγετε σωστά τα στοιχεία σας!"
+	if date_input.text.length() != 10 || name_input.text.length() == 0:
+		show_error("Παρακαλώ εισάγετε σωστά τα στοιχεία σας!")
 		return
 
-	if $CenterContainer/Menu/PlayerLabel.text.contains("1"):
-		player_name_1 = $CenterContainer/Menu/InputForm/NameIN.text
-		birth_date_ = $CenterContainer/Menu/InputForm/DateIN.text
-		$CenterContainer/Menu/PlayerLabel.text = "Παίκτης 2"
-		$CenterContainer/Menu/Buttons/Confirm.text = "Ξεκίνα!"
-		$CenterContainer/Menu/InputForm/NameIN.text =''
-		$CenterContainer/Menu/InputForm/DateIN.text =''
+	if player_label.text.contains("1"):
+		player_name_1 = name_input.text
+		birth_date_ = date_input.text
+		player_label.text = "Παίκτης 2"
+		confirm_button.text = "Ξεκίνα!"
+		name_input.text = ''
+		date_input.text = ''
 		return
 
-	player_name_2 = $CenterContainer/Menu/InputForm/NameIN.text
-	birthdate_2 = $CenterContainer/Menu/InputForm/DateIN.text
+	player_name_2 = name_input.text
+	birthdate_2 = date_input.text
 	var player_names := [player_name_1, player_name_2]
 	var birthdates := [birth_date_, birthdate_2]
 	# Set new game data
@@ -54,10 +71,10 @@ func _on_confirm_pressed() -> void:
 	get_tree().change_scene_to_file("res://Levels/Lvl1_1.tscn")
 
 func _on_back_pressed() -> void:
-	$CenterContainer/Menu/InputForm/NameIN.text =''
-	$CenterContainer/Menu/InputForm/DateIN.text =''
-	$CenterContainer/Menu.visible = true
-	$"CenterContainer/Pop-Up".visible = false
+	name_input.text = ''
+	date_input.text = ''
+	menu_container.visible = true
+	popup.visible = false
 	return
 
 func _on_back_to_menu_pressed() -> void:
